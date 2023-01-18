163fc96f5367d386c1382fcd600975dbf4cb2802
tompston <tompston@protonmail.com>
2023-01-18 16:59:16 +0200

    inputs are seperate components that emit the value on change

--- frontend/src/components/global/InputComponent.vue
+++ frontend/src/components/global/InputComponent.vue
@@ -10,13 +10,17 @@ defineProps<{
     inputType: string
     inputClass?: string
     inputId: string
+    inputValue: string | number | boolean
 }>()
+
+defineEmits(["update"]);
 
 </script>
 
 <template>
     <div :class="containerClass">
         <label :for="inputId" :class="labelClass">{{ labelName.toUpperCase() }}</label>
-        <input :type="inputType" :class="inputClass" :id="inputId">
+        <input :type="inputType" :class="inputClass" :id="inputId" :value="inputValue"
+            @input="$emit('update', $event.target.value)">
     </div>
 </template>
--- frontend/src/components/pages/Authenticate/SignupForm.vue
+++ frontend/src/components/pages/Authenticate/SignupForm.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 import { StorageKey, Auth } from "../../../assets/ts"
 import { useRouter } from 'vue-router';
 import * as F from "../../../../../backend/public/gomarvin.gen"
+import InputComponent from '../../global/InputComponent.vue';
 
 // router variable
 const router = useRouter()
@@ -13,13 +14,11 @@ const email = ref<string>("")
 // State Variables
 const isFetching = ref<boolean>(false)
 const apiResponseFailed = ref<boolean>(false)
-const error_message = ref<string>("")
+const failedValidationFields = ref<[]>([])
 
 /** Full flow for loggingg in a user */
 async function PostUserLoginDetails() {
     isFetching.value = true
-    error_message.value = ""
-    // error_message.value = ""
 
     const res = await F.UserEndpoints.RegisterUser(
         F.defaultClient, {
@@ -28,24 +27,22 @@ async function PostUserLoginDetails() {
         email: email.value
     })
 
-    /** 
-     * If the response is ok:
-     *      Save the returned access token in localstorage 
-     *      and move to the default homepage which needs 
-     *      a correct authentication token to return
-     *      the date from an authenticated API endpoint
-     * If response is not ok
-     *      return error message
-     */
-    if (res.status === 200) {
+    const data = await res.json()
+
+    if (data.status === 200) {
         isFetching.value = false
-        const data = await res.json()
         StorageKey.Set(Auth.ACCESS_TOKEN_KEY(), data.data.token.access_token)
         router.push({ path: '/' })
-    } else {
+    }
+    /** If payload validation fails  */
+    else if (data.status === 400) {
+        failedValidationFields.value = data.data
+        isFetching.value = false
+    }
+    /** If the response fails completely */
+    else {
         isFetching.value = false
         apiResponseFailed.value = true
-        // error_message.value = 
     }
 }
 
@@ -57,26 +54,13 @@ async function PostUserLoginDetails() {
         <form @submit.prevent>
 
             <div class="grid gap-3">
-
-                <!-- Email -->
-                <div>
-                    <label for="email-login" class="input__1-label">EMAIL</label>
-                    <input required type="text" class="input__1" placeholder="username" id="email-login"
-                        v-model="email">
-                </div>
-                <!-- Username -->
-                <div>
-                    <label for="username-login" class="input__1-label">USERNAME</label>
-                    <input required type="text" class="input__1" placeholder="username" id="username-login"
-                        v-model="username">
-                </div>
-                <!-- Password -->
-                <div>
-                    <label for="password-login" class="input__1-label">PASSWORD</label>
-                    <input required type="password" class="input__1" placeholder="password" id="password-login"
-                        v-model="password">
-                </div>
-                <div><button class="button__1" @click="PostUserLoginDetails()">LOG IN</button></div>
+                <InputComponent labelClass="input__1-label" inputClass="input__1" labelName="EMAIL" inputType="text"
+                    inputId="email-login" :inputValue="email" @update="(email = $event)" />
+                <InputComponent labelClass="input__1-label" inputClass="input__1" labelName="USERNAME" inputType="text"
+                    inputId="username-login" :inputValue="username" @update="(username = $event)" />
+                <InputComponent labelClass="input__1-label" inputClass="input__1" labelName="PASSWORD" inputType="text"
+                    inputId="password-login" :inputValue="password" @update="(password = $event)" />
+                <div><button class="button__1" @click="PostUserLoginDetails()">SIGN UP</button></div>
             </div>
         </form>
     </div>
